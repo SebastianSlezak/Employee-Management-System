1cff7067f87279ef9fc64dc614760308e6f7e2e1
Sebastian <szalassje@gmail.com>
2024-01-20 19:23:07 +0100

    Fill and reserve have been added.

--- JippVector/JippVector.cpp
+++ JippVector/JippVector.cpp
@@ -6,6 +6,22 @@ using namespace std;
 
 int main()
 {
-    
-}
+	JippVector<int> intVector(8);
+	intVector.size = 8;
 
+	intVector.fill(0);
+
+	for (size_t i = 0; i < intVector.size; i++)
+	{
+		cout << intVector.data[i] << endl;
+	}
+
+	cout << endl;
+
+	intVector.fill(2, 3, 997);
+
+	for (size_t i = 0; i < intVector.size; i++)
+	{
+		cout << intVector.data[i] << endl;
+	}
+}
--- JippVector/JippVector.h
+++ JippVector/JippVector.h
@@ -1,1 +1,60 @@
 #pragma once
+#ifndef JIPPVECTOR_H
+#define JIPPVECTOR_H
+
+using namespace std;
+
+template <typename T>
+class JippVector
+{
+private:
+
+public:
+	T* data;
+	int size;
+	int capacity;
+
+	JippVector() {
+		data = nullptr;
+		capacity = size = 0;
+	}
+
+	explicit JippVector(const size_t& capacity) {
+		data = new T[capacity];
+		this->capacity = capacity;
+		this->size = 0;
+	}
+
+	~JippVector() {
+		delete[] data;
+	}
+
+	void reserve(const int& val) {
+		if (capacity < val) {
+			int lenOldData = sizeof(data) / sizeof(data[0]);
+			capacity = val;
+			T* tempData = new T[capacity];
+			copy(data, data + min(lenOldData, capacity), tempData);
+			delete[] data;
+			data = tempData;
+		}
+	}
+
+	void fill(const int& val) {
+		for (int i = 0; i < size; i++) {
+			data[i] = val;
+		}
+	}
+
+	void fill(const int& start, const int& len, const int& val) {
+		for (int i = start; i < start + len; i++) {
+			data[i] = val;
+		}
+	}
+
+	void pushBack(const int& val) {
+		
+	}
+};
+
+#endif // JIPPVECTOR_H
